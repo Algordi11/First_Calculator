722803ee7d0d84430dc747da78c7c07a9db7ff4d
Algordi11 <gorelin.alexander@gmail.com>
2023-02-12 17:50:19 +0100

    rechenfehler behoben

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ print ('                                                                        
 print ('                                                                                                                                                                                                                          ')
 print ('ERKLÄHRUNG: tippe auf eine oder mehrere ziffern auf der Tastatur, dann auf ein rechenzeichen (Plus, minus, mal, geteilt durch). Anschliesend drückst du wieder eine oder mehrere ziffern, drückst Enter und das ergebniss erscheint.')
 print ('                                                                                                                                                                                                                          ')
-zahl1 = input('geben sie die erste zahl ein.')
-a = zahl1
+zahl1 = int(input('geben sie die erste zahl ein.'))
 zeichen = input('geben sie ein mathematisches Symbol ein.')
-zahl2 = input ('geben sie die zweite zahl ein')
-b = zahl2
+zahl2 = int(input ('geben sie die zweite zahl ein'))
 if zeichen == '+':
-    print (a + b)
+    print (zahl1 + zahl2)
 elif zeichen == '-':
-    print (a-b)
+    print (zahl1 - zahl2)
 elif zeichen == '*':
-    print (a*b)
+    print (zahl1 * zahl2)
 elif zeichen == '/':
-    print (a/b)
+    print (zahl1 / zahl2)
 else:
     print ('diese mathematische operation ist mir noch nicht bekannt')
